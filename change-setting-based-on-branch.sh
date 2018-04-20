11a86f57b49246be4c20477f042c9b127623253c
motiko <moti.kor@gmail.com>
2018-04-20 09:21:15 +0700

    print previous username

--- change-setting-based-on-branch.sh
+++ change-setting-based-on-branch.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # .git/hooks/post-checkout
 branch=$(git status --short -b | cut -d' ' -f2-)
+previous=$(git config user.name)
 case $branch in
   personal*)
     git config user.name "Personal Name"
-    echo "changed user.name to Personal Name"
+    echo "changed user.name from $previous to Personal Name"
     ;;
   master*)
     git config user.name "Company Name"
-    echo "changed user.name to Company Name"
+    echo "changed user.name from $previous to Company Name"
     ;;
   *)
     echo "Some other branch"
